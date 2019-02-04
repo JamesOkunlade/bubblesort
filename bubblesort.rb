f79fedf050d23581ccb2893afd787e03c8d7fddc
Lade James <ljblunts@gmail.com>
2019-02-04 11:17:16 +0100

    first attempt before testing

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,4 +1,17 @@
-def bubblesort
-  
-
+def bubblesort(array)
+  i = array.length
+  while (i > 0)
+    j = 0
+    while (j < i)
+      if (array[j] > array[j + 1]) {
+        temp = array[j]
+        array[j] = array[j + 1]
+        array[j + 1] = temp
+      }
+      end
+      j += 1
+    end
+    i -= 1
+  end
+  return array
 end
